92cc70732a24077a2e6a72697136370557bc0183
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-04-19 15:25:24 -0500

    read in data

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
+
+# Finding the path to file
+for dirpath, _, file_names in os.walk("data/"):
+    for filename in file_names:
+        print(os.path.join(dirpath, filename))
+
+
+training = pd.read_csv('data/train.csv')
