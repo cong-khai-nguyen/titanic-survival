b4f301a538a5066740b84b96464adf4a9b733f92
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-04-20 13:43:24 -0500

    Find correlation between independent variables to avoid multicollinearity

--- main.py
+++ main.py
@@ -19,8 +19,25 @@ test['train_test'] = 0
 test['Survived'] = np.NaN
 all_data = pd.concat([training,test])
 
-# print(all_data.columns)
+print(all_data.columns)
 
-# Take a closer look at the data given
-print(training.info())
-print(training.describe())
+# Take a closer look at the data given (data types and number of null values)
+# print(training.info())
+# print(training.describe())
+
+# look at numeric and categorical values separately
+df_num = training[['Age','SibSp','Parch','Fare']]
+df_cat = training[['Survived','Pclass','Sex','Ticket','Cabin','Embarked']]
+
+# for i in df_num.columns:
+#     plt.hist(df_num[i])
+#     plt.title(i)
+#     plt.show()
+
+# Find correlation between independent variables
+# print(df_num.corr())
+# sns.heatmap(df_num.corr(), annot=True)
+# plt.show()
+
+# compare survival rate across Age, SibSp, Parch, and Fare
+print(pd.pivot_table(training, index = 'Survived', values = ['Age','SibSp','Parch','Fare']))
